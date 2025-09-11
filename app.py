f5fb79840d326b9df263722d231e655f0bc40974
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-10 22:40:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ root = Tk()
 root.title("GameServer")
 root.geometry("400x400")
 
+widget_text = Label(text="¡Hola Bienvenido(a) al juego!", fg="lightgreen")
+widget_text.place(x=280, y=60)
+
+widget_btn = Button(text="Botón")
+
 root.mainloop()
