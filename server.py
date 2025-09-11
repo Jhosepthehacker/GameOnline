cf29cc203e8a5ae27f1ade5b387e3f58cab7c703
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-11 11:21:28 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import sys
 
 HOST = 'localhost'
 PORT = 8080
@@ -21,6 +22,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 message = data.decode()
 
             except KeyborardInterrupt:
-                pass
+                sys.exiimport socket
+import threading
+
+HOST = 'localhost'
+PORT = 8080
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(5)
+
+    while True:
+        try:
+            try:
+                conn, addr = s.accept()
+
+                print(f"Conexión Exitosa con: {addr}")
+
+                s.sendall("¡Hola, desde el servidor!".encode('utf-8'))
+      
+                data = s.recv(1024)
+                message = data.decode()
+
+            except KeyboardInterrupt:
+                sys.exit()
         except OSError:
             pass
