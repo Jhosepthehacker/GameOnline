cfd56c489293c1cf58087db65730f8265c21f0c5
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-10 22:52:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import socket
 from tkinter import *
 
+class ConnectServer:
+
+    def __init__(self):
+        self.HOST = 'localhost'
+        self.PORT = 8080
+
+def connect_server(self):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((self.HOST, self.PORT))
+
+        while True:
+            self.data_receive = s.recv(1024)
+            self.message = self.data_receive.decode()
+
+            print(f"Mensaje recibido: {self.message}")
+
+            s.sendall("¡Hola, desde el cliente!".encode('utf-8'))
+
 root = Tk()
 root.title("GameServer")
 root.geometry("400x400")
