4a9da0b03a94014bb2c10054732a7039bbac7642
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-10 23:38:39 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = 'localhost'
+PORT = 8080
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(5)
+
+    while True:
+        try:
+            try:
+                conn, addr = s.accept()
+
+                print(f"Conexión Exitosa con: {addr}")
+
+                s.sendall("¡Hola, desde el servidor!".encode('utf-8'))
+      
+                data = s.recv(1024)
+                message = data.decode()
+
+            except KeyborardInterrupt:
+                pass
+        except OSError:
+            pass
