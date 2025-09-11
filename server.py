fab3055694b99f9cda05acd6cf2f0dc08dc1e4a5
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-11 11:41:14 -0400

    Update server.py

--- server.py
+++ server.py
@@ -5,12 +5,16 @@ import sys
 HOST = 'localhost'
 PORT = 8080
 
-try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(5)
-except OSError:
-    pass
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen(5)
+        break
+    
+    except OSError:
+        PORT = 6785
+        continue
     
     while True:
         try:
