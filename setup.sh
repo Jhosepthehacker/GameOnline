b816c9e3358ab6a427a0d014b33c346eae3797cc
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-16 17:43:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,25 @@ echo "${GREEN_COLOR}            SETUP            "
 echo "${BLUE_COLOR}-----------------------------\n"
 
 apt install python3
+sleep 1
+
+apt install python-tk
+sleep 1
+
 apt install pip
+sleep 1
+
+# Aúnque hayan librerías preinstaladas en python, igualmente hay que asegurarse de instalarlas bien
+
+pip install kivy
+sleep 1
+
+pip install sockets
+sleep 1
+
+pip install time
+sleep 1
+
+pip install threading
+
+# Estoy ejecutando mucho "pip", se puede usar coma por cada librería o framework que se quiera instalar??
