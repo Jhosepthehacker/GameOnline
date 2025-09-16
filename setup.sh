d331f2d04fda5bd4c2c4a6af370345aa8bef0982
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-16 17:09:36 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+BLUE_COLOR= "\e[34m"
+GREEN_COLOR= "\e[33m"
+
+echo "${BLUE_COLOR}-----------------------------"
+echo "${GREEN_COLOR}            SETUP            "
+echo "${BLUE_COLOR}-----------------------------\n"
+
+apt install python3
+apt install pip
