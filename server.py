0f2e95545a44ea831be7a72a30f1ce81078530e6
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-28 13:16:02 -0400

    Update server.py

--- server.py
+++ server.py
@@ -4,9 +4,11 @@ import sqlite3 as sql  # Para manejar la base de datos SQLite
 import threading as thread  # Para trabajar con hilos (aunque no se usa aún)
 import sys         # Para poder salir del programa con sys.exit()
 
-# Configuración inicial del servidor
-HOST = 'localhost'  # Dirección del servidor (en este caso, la misma máquina)
-PORT = 8080         # Puerto en el que se intentará levantar el servidor
+# Anque podría heredar de la clase en el archivo 'app.py', sin embargo quiero hacerla independiente
+
+class accept_Conection:
+    def __init__(self):
+        pass
 
 # Clase encargada de manejar la base de datos
 class DataBase:
@@ -39,44 +41,3 @@ class DataBase:
         self.conn.commit()
         # Cerramos la conexión
         self.conn.close()
-
-# Intentamos iniciar el servidor en el puerto configurado
-while True:
-    try:
-        # Creamos el socket con IPv4 y TCP
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            # Asociamos el socket al host y puerto
-            s.bind((HOST, PORT))
-            # Ponemos el servidor en modo escucha (máx. 5 conexiones en cola)
-            s.listen(5)
-        break  # Si todo salió bien, salimos del bucle
-    
-    except OSError:
-        # Si el puerto 8080 está ocupado, cambiamos al 6785
-        PORT = 6785
-        print("Hubo un pequeño error")
-        continue  # Reintentamos con el nuevo puerto
-
-    # Bucle principal para aceptar conexiones
-    while True:
-        try:
-            try:
-                # Esperamos a que un cliente se conecte
-                conn, addr = s.accept()
-                print(f"Conexión Exitosa con: {addr}")
-
-                # Enviamos un mensaje al cliente
-                s.sendall("¡Hola, desde el servidor!".encode('utf-8'))
-      
-                # Recibimos datos del cliente (hasta 1024 bytes)
-                data = s.recv(1024)
-                # Decodificamos el mensaje recibido
-                message = data.decode()
-
-            except KeyboardInterrupt:
-                # Si el usuario presiona Ctrl+C, salimos
-                sys.exit()
-
-        except OSError:
-            # Si ocurre un error en la comunicación
-            print("Hubo un error en la comunicación")
