1a5f60aaefd313718afc575123f0d19c4e754b5c
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-28 13:37:49 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const messageWarning = "¡Si no usas CommonJs, no podrás interactuar con este juego!";
+
+console.log(messageHello);
+console.log("");
+
+const userResponse = prompt("¿Cómo te llamas?: ");
+console.log(`¡Qué bonito nombre ${userResponse}`);
+console.log("");
+
+const questionForTheUser = prompt("¿?")
