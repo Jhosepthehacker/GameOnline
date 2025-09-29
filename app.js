20a016aeb90704b0162ebdc971949faf6b5773fa
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-09-29 09:18:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 function game(){
-const messageWarning = "¡Si no usas CommonJs, no podrás interactuar con este juego!";
+  const readLineSync = require('readline-sync');
+  const messageWarning = "¡Si no usas CommonJs, no podrás interactuar con este juego!";
 
-console.log(messageHello);
-console.log("");
+  console.log(messageHello);
+  console.log("");
 
-const userResponse = prompt("¿Cómo te llamas?: ");
-console.log(`¡Qué bonito nombre ${userResponse}`);
-console.log("");
+  const userName = readLineSync.question("¿Cómo te llamas?: ");
+  console.log(`¡Qué bonito nombre ${userName}!`);
+  console.log("");
 
-const questionForTheUser = prompt("¿Quiéres jugar a adivinar un número del 1 al 100?: ")
+  const questionForTheUser = prompt("¿Quiéres jugar a adivinar un número del 1 al 100?: ")
 
-// Se comprueban dos 'si', uno con acento y otro sin acento (para aclarar la confusión en caso de que si la hay)
+  // Se comprueban dos 'si', uno con acento y otro sin acento (para aclarar la confusión en caso de que si la hay)
 
-if (questionForTheUser === "sí" || questionForTheUser === "si"){
-  console.log("Empezando el juego....");
+  if (questionForTheUser === "sí" || questionForTheUser === "si"){
+    console.log("Empezando el juego....");
 
-  const number = undefined;
-  let trys = null;
+    const number = undefined;
+    let trys = null;
 
-  while true{
-    let answerOfUser = prompt("Adivina un número del 1 al 100");
-    trys += 1;
+    while true{
+      let answerOfUser = prompt("Adivina un número del 1 al 100");
+      trys += 1;
     
-    console.log("");
+      console.log("");
     
-    if (answerOfUser > number){
-      console.log(`El número ${answerOfUser} es demasiado alto`);
+      if (answerOfUser > number){
+        console.log(`El número ${answerOfUser} es demasiado alto`);
     }
-    else if (answerOfUser < number){
-      console.log(`El número ${answerOfUser} es demasiado bajo`);
+      else if (answerOfUser < number){
+        console.log(`El número ${answerOfUser} es demasiado bajo`);
     }
-    else if (answerOfUser === number){
-      console.log(`Felicidades has encontrado el número ${answerOfUser}, en ${trys} intentos`);
+      else if (answerOfUser === number){
+        console.log(`Felicidades has encontrado el número ${answerOfUser}, en ${trys} intentos`);
     }
 }
 }
