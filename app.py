84d5030f1f2f8b825fbade089e7b6302caec43fc
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-10-30 23:01:54 -0400

    Refactor game_in_terminal function and add options

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # ==========================
 import socket               # Para manejar la conexión cliente-servidor
 import sys                  # Para salir del programa si es necesario
+import os
 import sqlite3 as sql       # Para manejar la base de datos SQLite  # Para posibles hilos (no usado aún)
 from tkinter import *       # Para la interfaz gráfica
 from tkinter import ttk, messagebox, Toplevel
@@ -98,33 +99,181 @@ class ConnectServer:
         except OSError as e:
             print(f"Error en la conexión: {e}")
 
-def game_in_terminal():
-    try:
+def game_in_terminal(option):
+    if option == "1":
         try:
-            number = random.randint(1, 100)
-            trys = 0
-            while True:
-                try:
-                    number_to_usr = input(f"{user_name} adivina un número del 1 al 100: ")
-                    trys += 1
-                except ValueError:
-                    print("Ingresa un número, no texto")
-                    continue
+            try:
+                number = random.randint(1, 100)
+                trys = 0
+                while True:
+                    try:
+                        number_to_usr = input(f"{user_name} adivina un número del 1 al 100: ")
+                        trys += 1
+                    except ValueError:
+                        print("Ingresa un número, no texto")
+                        continue
 
-                if number_to_usr < number:
-                    print(f"El número {number_to_usr} es demasiado bajo al número secreto")
-                elif number_to_usr > number:
-                    print(f"El número {number_to_usr} es demasiado alto al número secreto")
-                elif number_to_usr == number:
-                    print(f"Felicidades {user_name}, has encontrado el número {number} en {trys} intentos")
-                    break
-        except NameError:
-            print("Ha ocurrido un problema")
-            sleep(1)
-            sys.exit()
+                    if number_to_usr < number:
+                        print(f"El número {number_to_usr} es demasiado bajo al número secreto")
+                    elif number_to_usr > number:
+                        print(f"El número {number_to_usr} es demasiado alto al número secreto")
+                    elif number_to_usr == number:
+                        print(f"Felicidades {user_name}, has encontrado el número {number} en {trys} intentos")
+                        break
+            except NameError:
+                print("Ha ocurrido un problema")
+                sleep(1)
+                sys.exit()
 
-    except EOFError:
-        print("No estás en un entorno interactivo de terminal o consola")
+        except EOFError:
+            print("No estás en un entorno interactivo de terminal o consola")
+    elif option == "2":
+        import random
+        import sqlite3 as sql
+
+        class DataBase:
+        def __init__(self, conn, position, inventory):
+            self.conn = conn
+            self.conn.commit()
+            self.conn.close()
+
+            self.create_table()
+            self.insert_data(position, inventory)
+
+        def create_table(self):
+            try:
+                self.conn = sql.connect("game_logs.db")
+                self.cursor = self.conn.cursor()
+                self.cursor.execute(
+		            """CREATE TABLE IF NOT EXISTS logs(
+		                position TEXT,
+		                inventory TEXT
+		    )"""
+)
+
+            finally:
+                self.conn.commit()
+                self.conn.close()
+
+        def insert_data(self, position, inventory):
+            try:
+                self.conn = sql.connect("game_logs.db")
+                self.cursor = self.conn.cursor()
+                self.instruction = f"INSERT INTO logs VALUES('{position}', '{inventory}')"
+                self.cursor.execute(self.instruction)
+            finally:
+                self.conn.commit()
+                self.conn.close()
+
+         def read_data(self):
+             try:
+                 self.conn = sql.connect("game_logs.db")
+                 self.instruction = "SELECT * FROM logs;"
+                 self.cursor = self.conn.cursor()
+                 self.cursor.execute(self.instruction)
+                 data = self.cursor.fetchall()
+
+                 return data
+            finally:
+                self.conn.commit()
+                self.conn.close()
+
+    map = [
+       list("  ###########    #########"),
+       list("  # @       #####        #"),
+       list("  #     E            H   #"),
+       list("  #         #####        #"),
+       list("  ##########     #########")
+]
+
+    for i in map:
+        print(len(i))
+
+    print(
+           """
+  @ = Tu personaje (6 Vidas)
+  E = Enemigo (4 Vidas)
+  H = Hierro (Mina)
+  
+  // Escribe 'i' para ver tu inventario //
+  // Escribe 'x' para ocultar tu inventario //
+           """
+
+)
+
+    player_row = 1
+    player_column = 4
+
+    lifes = 6
+    lifes_enemy = 4
+
+    inventory = []
+    position = map
+
+    def print_map():
+	    for i in map:
+            print("".join(i))
+
+	    print(f"\nVidas: {lifes}")
+	    print("\n------\n")
+
+    def move_character(direction):
+	    global player_row, player_column, lifes, lifes_enemy
+	
+        row, column = player_row, player_column
+	
+	    if direction == "w":
+		    row -= 1
+        elif direction == "s":
+		    row += 1
+	    elif direction == "a":
+		    column -= 1
+	    elif direction == "d":
+		    column += 1
+	
+	    next_row, next_column = row, column
+	
+	    if map[next_row][next_column] != "#":
+            if map[next_row][next_column] == "E":
+			    possible_attack = random.randint(1, 6)
+
+                if possible_attack in (1, 2, 3):
+                    lifes -= 1
+                    print("Has perdido 1 vida\n")
+			    elif possible_attack in (4, 5, 6):
+                    lifes_enemy -= 1
+				    print("El enemigo ha perdido una vida")
+			
+		    else:
+                 map[player_row][player_column] = ' '
+                 map[next_row][next_column] = '@'
+		
+                 player_row, player_column = next_row, next_column
+        else:
+             print("No puedes atravesar la barrera (límite del juego)")
+
+    while True:
+	    print_map()
+	
+        move_player = input("Escribe (w/s/a/d) para moverte, 'q' para salir: ").lower().strip()
+
+        if move_player in ("w", "s", "a", "d"):
+		    move_character(move_player)
+		    time.sleep(1)
+		    os.system('clear')
+	    elif move_player == "q":
+		    time.sleep(1)
+		    sys.exit()
+	    elif move_player == "i":
+	        print("Inventario:\n")
+	    
+	        for i in inventory:
+	            print(f"  {i}")
+	    elif move_player == "x":
+		    pass
+	    else:
+		    print("Ingresa un caracter válido")
+        
 
 # ==========================
 #   Interfaz / Terminal
@@ -172,10 +321,20 @@ try:
             root.mainloop()
         else:
           # Si no, cargamos la versión en terminal
-            print("\nEl juego se generará en la terminal. Por favor espere un momento....")
+            print("\nEl juego se generará en la terminal. Por favor espere un momento....\n")
             sleep(1)
 
-            game_in_terminal()
+            while True
+            
+                print("""
+  ✓ Adivina un número del 1 al 100 (Escribe "1")
+  ✓ Mapa y exploración (Escribe "2")
+            """
+)
+            
+                question = input("Elige un juego: ").strip()
+            
+                game_in_terminal(question)
 
     except EOFError:
         try:
