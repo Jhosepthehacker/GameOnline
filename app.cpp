ad7255e3fc69cc810cc15abbdaa2c1b74801d619
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-08 13:10:11 -0400

    Add number guessing game functionality
    Implement a number guessing game with user input.

--- app.cpp
+++ app.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
+void game() {
+  int userPlaying;
+  int number = 20;
+  int trys = 0;
+
+  while (true) {
+    std:: cout << "Adivina un número del 1 al 100: ";
+    std:: cin >> userPlaying;
+
+    if (userPlaying < number) {
+      std:: cout << "Demasiado bajo" << std:: endl;
+    }
+    else if (userPlaying < number) {
+      std:: cout << "Demasiado alto" << std:: endl;
+    }
+    else if (userPlaying == number) {
+      std:: cout << "Felicidades has encontrado el número " << number << " en " << trys << " intentos" << std:: endl;
+      break;
+  }
+}
+
 int main() {
-  std:: cout << "En desarrollo...." << std:: endl;
+  game();
 
   return 0;
 }
