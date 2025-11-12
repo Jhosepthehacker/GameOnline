42f6b88ac01ab81a209ebfb6f9124989432174ff
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-11 22:46:04 -0400

    Implement greeting and user interaction in Spanish
    Added greeting message and user input prompt in Spanish.

--- app.cpp
+++ app.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 
+// Proximamente se implementará "SDL2"
+
+namespace message {
+  std:: string hello = "Hola Usuario";
+}
+
 void game() {
+  std:: string name;
+  
+  std:: cout << message:: hello << std:: endl;
+  std:: cout << "\n";
+
+  std:: cout << "¿Cómo te llamas?: ";
+  std:: cin >> name;
+  std:: cout << "\n";
+  
+  std:: cout << "Mucho gusto " + name << std:: endl;
+  
   int userPlaying;
-  int number = 20;
+  int number = 20; // Se está trabajando el problema del random (en desarrollo....)
   int trys = 0;
 
   while (true) {
     std:: cout << "Adivina un número del 1 al 100: ";
     std:: cin >> userPlaying;
-
-    std:: cout << "\n" << std:: endl;
+    std:: cout << "\n";
 
     if (userPlaying < number) {
       std:: cout << "Demasiado bajo" << std:: endl;
