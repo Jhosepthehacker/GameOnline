bd20d4abba5b6a230d1924c800451c61f8c6af54
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-12-12 09:33:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -220,10 +220,15 @@ try:
 
         if is_insert[0][0] < 1:
             for i in user_name:
-                if i in (';', '(', ')', "'"):
+                if i in (';', '(', ')', "'", '*'):
                     print("Solo se aceptan letras (no carácteres ni números)")
+					break
+                elif user_name.split().strip() in ("SELECT", "DROP", "WHERE", "UPDATE", "SET", "select", "drop", "where", "update", "set"):
+					print("No se aceptan comandos SQL en el input")
+					break
 				else:
                     app.sql_command(f"INSERT INTO user (id, name, trys) VALUES (1, '{user_name}', 1);")
+				    break
     
       # Preguntamos si está en un entorno gráfico
         response = input(f"\n¿{user_name}, usted está en un entorno gráfico (GUI)?: ").lower()
